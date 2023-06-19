3921a3d51f0be11bffa05db33dedc6b431463ccd
Reza Mashayekhi <rezamashayekhi22@yahoo.com>
2023-06-19 15:56:45 -0600

    Adds button

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Slider() {
+  return (
+    <button class='rectangle'>  </button>
+  );
+}
+
+function App() {
+  return (
+    <main>
+       <h1> Hi! you can play Ping Pong here.</h1>
+       <Slider />
+    </main>
+  );
+}
+
+export default App;
